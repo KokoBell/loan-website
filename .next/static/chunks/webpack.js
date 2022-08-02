ec2845887b5e5bc20713dd26579edeb77576adfe
Tumelo Koko <tumelokoko10@gmail.com>
2022-08-02 15:14:14 +0200

    First round of responsiveness

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5e1f3dd3814d7687"; }
+/******/ 		__webpack_require__.h = function() { return "00b6052625b71776"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/navbar.js
+++ components/navbar.js
@@ -3,18 +3,17 @@ import Button from './button'
 
 const NavBar = (props) => {
     const default_function = () => { console.log("Add your function here") }
-    return (<div className={styles.nav_background}>
-        <div className={styles.navbar}>
+    return (
             <div className={styles.nav_links}>
                 <h1 className={styles.nav_logo}> {props.logo} </h1>
                 <p className={styles.nav_link}> All Products </p>
                 <p className={styles.nav_link}> Learn </p>
                 <p className={styles.nav_link}> Resources </p>
                 <p className={styles.nav_link}> Contact </p>
-                <Button text="Sign Up" class_name="signup_button" function={default_function} />
+                <div className={styles.signup}>
+                <p className={styles.nav_link}> Sign Up </p>
+                </div>
             </div>
-        </div>
-    </div>
     )
 }
 
