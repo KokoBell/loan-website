abe5b8d6bf634731b1c8ffd4d37dabc237390a30
Tumelo Koko <tumelokoko10@gmail.com>
2022-08-04 16:00:34 +0200

    Add icons to investments section

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e5ec5537e7f4e719"; }
+/******/ 		__webpack_require__.h = function() { return "dc70a52c8505ac78"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
