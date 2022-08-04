2c27cc0b2f8d8688535847c4d0ae22f67093d5e2
Tumelo Koko <tumelokoko10@gmail.com>
2022-08-04 15:56:04 +0200

    Add product icons

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a5920167497ebf96"; }
+/******/ 		__webpack_require__.h = function() { return "e5ec5537e7f4e719"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/card.js
+++ components/card.js
@@ -1,14 +1,23 @@
 import styles from '../styles/Card.module.css'
 
 const Card = (props) => {
-    const heading = props.heading ? props.heading.toUpperCase() : "Insert a heading".toUpperCase()
-    return (<div className={styles.card}>
-        <p className={styles.heading}>{heading}</p>
-        <p className={styles.text}>{props.text ? props.text : "Text goes here"}</p>
-        <p className={styles.link}>
-            <a href={props.url ? props.url : "/"}>{props.link_text ? props.link_text : "Link text goes here"}</a><span className={styles.arrow}>&rarr;</span>
-        </p>
-    </div>
+    const heading = props.heading ? props.heading : "Insert a heading"
+    return (
+        <div>
+            <a href={props.url ? props.url : "/"} className={styles.card}>
+                <div>
+                    <p className={styles.heading}>{heading}</p>
+                    <p className={styles.text}>{props.text ? props.text : "Text goes here"}</p>
+                    <p className={styles.link}>
+                        {props.link_text ? props.link_text : "Link text goes here"}<span className={styles.arrow}>&rarr;</span>
+                    </p>
+                </div>
+                <div className={styles.icon}>
+                    {props.icon}
+                </div>
+            </a>
+        </div >
+
     )
 }
 
