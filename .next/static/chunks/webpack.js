13edcdfdaf94dd437053781cea92789ff4098298
Tumelo Koko <tumelokoko10@gmail.com>
2022-08-04 19:27:52 +0200

    Add hover effect

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "342702acb1a4ed88"; }
+/******/ 		__webpack_require__.h = function() { return "9f24a3876fd51086"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
